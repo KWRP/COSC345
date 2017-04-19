5f93936020db93620553959fd3e6399eb023eff6
MrSmoki <ryan.s.n.swanepoel@gmail.com>
2017-04-19 18:06:18 +1200

    find/create date

--- XMLwork.cpp
+++ XMLwork.cpp
@@ -21,6 +21,29 @@ bool Test(const char* day = 0, const char* month = 0, const char* year = 0){
 	return true;
 }
 
+bool create(const char* day = 0, const char* month = 0, const char* year = 0) {
+
+	tinyxml2::XMLDocument xmlDoc;
+
+	int yearInt = atoi(year);
+	int monthInt = atoi(month);
+	int dayInt = atoi(day);
+	
+	if (yearInt <= 0 || monthInt <= 0 || monthInt > 12 || dayInt <= 0 || dayInt >= 31) { return false; }
+	
+	else {
+
+		tinyxml2::XMLNode * elementYear = xmlDoc.NewElement(year);
+		xmlDoc.InsertFirstChild(elementYear);
+
+		tinyxml2::XMLElement * elementMonth = xmlDoc.NewElement(month);
+		elementYear->InsertEndChild(elementMonth);
+
+		tinyxml2::XMLElement * elementDay = xmlDoc.NewElement(day);
+		elementMonth->InsertEndChild(elementDay);
+	}
+}
+
 int main()
 {
 	if (Test() == true) {
@@ -29,5 +52,6 @@ int main()
 	else {
 		std::cout << "fail" << std::endl;
 	}
+	create();
 }
 
