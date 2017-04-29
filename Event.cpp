7f1229a2e0aa12bf438087ace16d166262003d7d
paulsouter <paul.souter@hotmail.co.nz>
2017-04-29 12:45:53 +1200

    Delete Event.cpp

--- Event.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Event.h"
-
-Event::Event(string title, string description, int strartTime, int duration) {
-	this->description = description;
-	this->title = title;
-	this->startTime = startTime;
-	this->duration = duration;
-}
